8d17f94bec9c9103edf9b2adfd244cd78366b4d0
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-03-12 19:36:57 +0300

    app-spec

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # This script is used to start the application
 pm2 start /var/www/road-events-server/src/index.js -n road-events -f
