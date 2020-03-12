b3137f8fed61be8928dbf1de5ed5314f2d5e0b1b
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-03-12 20:47:37 +0300

    app-spec

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # This script is executed before copying the source
+yum -y update
 
-npm install npm@latest -g
-npm cache clean -f
-sudo npm install n -g
-sudo n stable
+curl --silent --location https://rpm.nodesource.com/setup_13.x | bash -
+
+yum -y install nodejs
+
+npm install -g pm2
 pm2 update
 
 if [ -d "/var/www/road-events-server" ];then
