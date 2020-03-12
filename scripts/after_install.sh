3c08c292feed1df8af68c074200588271b8e8731
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-03-12 20:57:59 +0300

    app-spec

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,4 @@
 
 # This script is executed after the source is copied to the instances
 cd /var/www/road-events-server
-sudo npm install
+npm install
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script is used to start the application
-sudo pm2 start /var/www/road-events-server/src/index.js -n road-events -f
+pm2 start /var/www/road-events-server/src/index.js -n road-events -f
