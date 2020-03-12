be8d1a129c11ff3ef2e6f2f9f9090b22421cc589
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-03-12 17:57:49 +0300

    app-spec

---
+++ scripts/after_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script is executed after the source is copied to the instances
+cd /var/www/road-events-server
+npm install
---
+++ scripts/before_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is executed before copying the source
+
+npm install npm@latest -g
+npm cache clean -f
+sudo npm install n -g
+sudo n stable
+pm2 update
+
+if [ -d "/var/www/road-events-server" ];then
+    rm -rf /var/www/road-events-server
+    mkdir -p /var/www/road-events-server
+else
+    mkdir -p /var/www/road-events-server
+fi
