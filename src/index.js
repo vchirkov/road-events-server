0e0806c2ce0fedc234531e92dc0e247bcf6053b9
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-03-19 20:55:37 +0300

    https port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = require('./app');
 const {botContext} = require('./bot');
 
-const {PORT = 80} = process.env;
+const {PORT = 443} = process.env;
 
 botContext.start();
 app.listen(PORT, () => console.log(`Ready on port ${PORT}`));
