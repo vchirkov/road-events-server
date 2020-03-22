80ac1396b1bcd13ce99f9b0214b9c3f92a0d3365
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-03-22 13:43:43 +0300

    sudo for ubuntu

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 # This script is executed before copying the source
-sudo yum -y update
-
-curl -sL https://rpm.nodesource.com/setup_13.x | sudo bash -
-
-sudo yum -y install nodejs
-
-sudo npm install -g pm2
 sudo pm2 update
 
 if [ -d "/var/www/road-events-server" ];then
-    rm -rf /var/www/road-events-server
-    mkdir -p /var/www/road-events-server
+    sudo rm -rf /var/www/road-events-server
+    sudo mkdir -p /var/www/road-events-server
 else
-    mkdir -p /var/www/road-events-server
+    sudo mkdir -p /var/www/road-events-server
 fi
