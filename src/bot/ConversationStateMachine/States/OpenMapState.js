ca7007a6fa49fd38437322220a074beaec0af68a
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-04-02 20:17:13 +0300

    hash router fix

--- src/bot/ConversationStateMachine/States/OpenMapState.js
+++ src/bot/ConversationStateMachine/States/OpenMapState.js
@@ -14,7 +14,7 @@ module.exports = class OpenMapState extends State {
 
     async send({query_id, bot, user_id}) {
         if (query_id) {
-            bot.answerCallbackQuery(query_id, {url: `${APP_URL}/#auth/token/${user_id}`});
+            bot.answerCallbackQuery(query_id, {url: `${APP_URL}/#/auth/token/${user_id}`});
         }
     }
 };
